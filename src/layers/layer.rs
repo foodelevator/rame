ce8bd340b58bb370744eb442311dc7807d425d02
mathiasmagnusson <mathiasmagnussons@gmail.com>
2018-12-31 21:45:46 +0100

    A Layer now owns an EventListener, but that makes it so that you can't access the Layer from events

--- src/layers/layer.rs
+++ src/layers/layer.rs
@@ -1,11 +1,7 @@
 use crate::events::EventListener;
 
-pub trait Layer: EventListener {
+pub trait Layer {
 	fn on_attach(&mut self) {}
 	fn on_detach(&mut self) {}
-	fn on_update(&mut self) {}
-	// fn on_event(&mut self, _event: &Event) {}
-
-	#[cfg(debug_assertions)]
-	fn get_name(&self) -> &str;
+	fn get_event_listener(&mut self) -> &mut EventListener;
 }
