8a9cf2a6e1f28c837ff1d66e1bdc437a29acf621
mathiasmagnusson <mathiasmagnussons@gmail.com>
2019-01-01 00:11:05 +0100

    Fuck testing that can't do shit with state machines or stdout

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,3 @@ pub mod layers;
 pub mod logging;
 pub mod vecs;
 pub mod window;
-
-#[cfg(test)]
-mod tests {
-	#[allow(unused_imports)]
-	use super::*;
-	#[test]
-	fn it_works() {
-		assert!(true && !false);
-	}
-}
